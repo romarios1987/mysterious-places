8824ea8c002dd336defa42febb2d3d1cff4e6531
romarios1987 <romios047@gmail.com>
2018-03-19 17:25:27 +0200

    Stretching the admin template. Webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,43 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+/**
+ * Подключаем все файлы для бэкенда
+ */
+mix.styles([
+    'resources/assets/backend/bootstrap/css/bootstrap.min.css',
+    'resources/assets/backend/font-awesome/4.5.0/css/font-awesome.min.css',
+    'resources/assets/backend/ionicons/2.0.1/css/ionicons.min.css',
+    'resources/assets/backend/plugins/iCheck/minimal/_all.css',
+    'resources/assets/backend/plugins/datepicker/datepicker3.css',
+    'resources/assets/backend/plugins/select2/select2.min.css',
+    'resources/assets/backend/plugins/datatables/dataTables.bootstrap.css',
+    'resources/assets/backend/dist/css/AdminLTE.min.css',
+    'resources/assets/backend/dist/css/skins/_all-skins.min.css'
+], 'public/css/admin.css');
+
+mix.scripts([
+    'resources/assets/backend/plugins/jQuery/jquery-2.2.3.min.js',
+    'resources/assets/backend/bootstrap/js/bootstrap.min.js',
+    'resources/assets/backend/plugins/select2/select2.full.min.js',
+    'resources/assets/backend/plugins/datepicker/bootstrap-datepicker.js',
+    'resources/assets/backend/plugins/datatables/jquery.dataTables.min.js',
+    'resources/assets/backend/plugins/datatables/dataTables.bootstrap.min.js',
+    'resources/assets/backend/plugins/slimScroll/jquery.slimscroll.min.js',
+    'resources/assets/backend/plugins/fastclick/fastclick.js',
+    'resources/assets/backend/plugins/iCheck/icheck.min.js',
+    'resources/assets/backend/dist/js/app.min.js',
+    'resources/assets/backend/dist/js/demo.js',
+    'resources/assets/backend/dist/js/scripts.js'
+], 'public/js/admin.js');
+
+// Копируем шрифты в папку public
+mix.copy('resources/assets/backend/bootstrap/fonts', 'public/fonts');
+mix.copy('resources/assets/backend/dist/fonts', 'public/fonts');
+
+// Копируем картинки в папку public
+mix.copy('resources/assets/backend/dist/img', 'public/img');
+
+mix.copy('resources/assets/backend/plugins/iCheck/minimal/blue.png', 'public/css');
+
+
