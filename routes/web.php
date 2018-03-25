40abd067655fefa6633bcdd610520083990d1d06
romarios1987 <romios047@gmail.com>
2018-03-25 21:30:17 +0300

    Middleware routes

--- routes/web.php
+++ routes/web.php
@@ -22,17 +22,18 @@ Route::get('/tag/{slug}', 'HomeController@tag')->name('tag.show');
 Route::get('/category/{slug}', 'HomeController@category')->name('category.show');
 
 
+Route::group(['middleware' => 'auth'], function (){
+    Route::get('/logout', 'AuthController@logout');
+});
 
-Route::get('/register', 'AuthController@registerForm');
-Route::post('/register', 'AuthController@register');
+Route::group(['middleware' => 'guest'], function (){
+    Route::get('/register', 'AuthController@registerForm');
+    Route::post('/register', 'AuthController@register');
+    Route::get('/login', 'AuthController@loginForm');
+    Route::post('/login', 'AuthController@login');
+});
 
-Route::get('/login', 'AuthController@loginForm');
-Route::post('/login', 'AuthController@login');
-
-Route::get('/logout', 'AuthController@logout');
-
-
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'admin'], function () {
 
     Route::get('/', 'DashboardController@index')->name('index');
     Route::resource('/categories', 'CategoriesController');
