3e926c2d9706a8c8a84ee3e40df7b327833c2cc0
romarios1987 <romios047@gmail.com>
2018-03-30 21:15:34 +0300

    profile user

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,19 @@ Route::get('/tag/{slug}', 'HomeController@tag')->name('tag.show');
 Route::get('/category/{slug}', 'HomeController@category')->name('category.show');
 
 
+
+
+
 Route::group(['middleware' => 'auth'], function (){
+    Route::get('/profile', 'ProfileController@index');
+    Route::post('/profile', 'ProfileController@store');
     Route::get('/logout', 'AuthController@logout');
 });
 
 Route::group(['middleware' => 'guest'], function (){
     Route::get('/register', 'AuthController@registerForm');
     Route::post('/register', 'AuthController@register');
-    Route::get('/login', 'AuthController@loginForm');
+    Route::get('/login', 'AuthController@loginForm')->name('login');
     Route::post('/login', 'AuthController@login');
 });
 
